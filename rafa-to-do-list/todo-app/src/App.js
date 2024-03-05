df371347f694cb792e8ac055845066f95479665e
rafa <114372052+rafaelsutiono@users.noreply.github.com>
2024-03-05 10:45:22 +0700

    Update App.js

--- rafa-to-do-list/todo-app/src/App.js
+++ rafa-to-do-list/todo-app/src/App.js
@@ -111,7 +111,7 @@ function App () {
   return (
     <div className="App">
       <h1>My Todo List</h1>
-      <p align="center"><a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=12&pause=1000&color=E4E4E4&center=true&vCenter=true&random=false&width=435&lines=by%3A+Rafael+Sutiono" alt="Typing SVG" /></a></p>
+      <p align="center"><a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=12&pause=1000&color=E4E4E4&center=true&vCenter=true&random=false&width=435&lines=by%3A+Rafael+Sutiono+-+2602174535" alt="Typing SVG" /></a></p>
     
       <div className="todo-wrapper">
         <div className="todo-input">
